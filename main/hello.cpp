ff3e50a98023e474efa4f9bfa449b783c4f8cf87
vaishnavisuresh123 <125581778+vaishnavisuresh123@users.noreply.github.com>
2023-03-04 08:39:45 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World by PES2UG20CS814\n";
   cout << "Hello, Jenkins by PES2UG20CS814\n";
-  cout << "This is PES2UG20CS814- J section\n";
+  cout << "This is PES2UG20CS814- J section hello\n";
   return 0;
 }
